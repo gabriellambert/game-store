a34875ce57d18c6c0799b8c2a01bf1ebbcd756e3
Felipe Pereira dos Santos <felipe.au.pereira.santos@gmail.com>
2025-03-12 20:18:04 +1100

    Fix: Adicioniona companion object para guardar as strings que representam labels da BottomNavItems.kt em constantes

--- app/src/main/java/com/gabriel/game_store/presentation/navigation/BottomNavItems.kt
+++ app/src/main/java/com/gabriel/game_store/presentation/navigation/BottomNavItems.kt
@@ -14,22 +14,32 @@ import kotlinx.serialization.Serializable
 sealed class BottomNavItems<T>(val label: String, val icon: @Contextual ImageVector, val route: T) {
     @Serializable
     data object Home : BottomNavItems<HomeScreen>(
-        label = "Home",
+        label = HOME_LABEL,
         icon = Icons.Filled.Home,
         route = HomeScreen
     )
 
     @Serializable
     data object Search : BottomNavItems<SearchScreen>(
-        label = "Search",
+        label = SEARCH_LABEL,
         icon = Icons.Filled.Search,
         route = SearchScreen
     )
 
     @Serializable
     data object Favorites : BottomNavItems<FavoritesScreen>(
-        label = "Favorites",
+        label = FAVORITES_LABEL,
         icon = Icons.Filled.Favorite,
         route = FavoritesScreen
     )
+
+    companion object {
+        private const val HOME_LABEL = "Home"
+        private const val SEARCH_LABEL = "Search"
+        private const val FAVORITES_LABEL = "Favorites"
+    }
+
 }
+
+
+
