4bfd4139bf545c113210088c93982b9d221fc650
Bruno Barbosa <141725324+BrunoHPBarbosa@users.noreply.github.com>
2025-03-18 15:42:16 +0000

    Refactor bottom nav. removido pngs e add svg par aicones no bottomnav

--- app/src/main/java/com/gabriel/game_store/presentation/navigation/BottomNavItems.kt
+++ app/src/main/java/com/gabriel/game_store/presentation/navigation/BottomNavItems.kt
@@ -8,19 +8,19 @@ sealed class BottomNavItems<T>(val label: String, val icon: Int, val route: T) {
     @Serializable
     data object Home : BottomNavItems<HomeScreen>(
         label = HOME_LABEL,
-        icon = R.drawable.home_icon,
+        icon = R.drawable.home_icon_svg,
         route = HomeScreen
     )
     @Serializable
     data object Search : BottomNavItems<SearchScreen>(
         label = SEARCH_LABEL,
-        icon = R.drawable.search_icon,
+        icon = R.drawable.search_icon_svg,
         route = SearchScreen
     )
     @Serializable
     data object Favorites : BottomNavItems<FavoritesScreen>(
         label = FAVORITES_LABEL,
-        icon = R.drawable.save_icon,
+        icon = R.drawable.saved_icon_svg,
         route = FavoritesScreen
     )
     companion object {
