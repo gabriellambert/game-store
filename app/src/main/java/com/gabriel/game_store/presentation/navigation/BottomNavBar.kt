e407ee288140ad46855b9e5103229712246ace8e
Bruno Barbosa <141725324+BrunoHPBarbosa@users.noreply.github.com>
2025-03-18 16:12:42 +0000

    Refactor bottom nav. removido pngs e add svg par aicones no bottomnav removido espaço

--- app/src/main/java/com/gabriel/game_store/presentation/navigation/BottomNavBar.kt
+++ app/src/main/java/com/gabriel/game_store/presentation/navigation/BottomNavBar.kt
@@ -5,15 +5,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
@@ -31,7 +27,6 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -57,79 +52,76 @@ fun BottomNavBar(
             .clip(shape = RoundedCornerShape(20.dp)),
         containerColor = Color.DarkGray.copy(alpha = 0.5f)
     ) {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentDestination = navBackStackEntry?.destination
 
-            val navBackStackEntry by navController.currentBackStackEntryAsState()
-            val currentDestination = navBackStackEntry?.destination
-
-            bottomNavDestinations.forEach { destination ->
-                val isSelected = currentDestination?.route == destination.route::class.qualifiedName
-                NavigationBarItem(
-                    modifier = Modifier,
-                    selected = isSelected,
-                    onClick = {
-                        navController.navigate(destination.route) {
-                            popUpTo(navController.graph.findStartDestination().id) {
-                                saveState = true
-                            }
-                            launchSingleTop = true
-                            restoreState = true
+        bottomNavDestinations.forEach { destination ->
+            val isSelected = currentDestination?.route == destination.route::class.qualifiedName
+            NavigationBarItem(
+                modifier = Modifier,
+                selected = isSelected,
+                onClick = {
+                    navController.navigate(destination.route) {
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
                         }
-                    },
-                    icon = {
-                        Column(
-                            horizontalAlignment = Alignment.CenterHorizontally,
-                            verticalArrangement = Arrangement.Bottom,
-                            modifier = Modifier
-                                .weight(1f)
-                                .padding(bottom = 4.dp)
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                },
+                icon = {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Bottom,
+                        modifier = Modifier
+                            .weight(1f)
+                            .padding(bottom = 4.dp)
+                    ) {
+                        Row(
+                            horizontalArrangement = Arrangement.Center,
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier.fillMaxWidth()
                         ) {
-                            Row (
-                                horizontalArrangement = Arrangement.Center,
-                                verticalAlignment = Alignment.CenterVertically,
-                                modifier = Modifier.fillMaxWidth()
-                            ){
-                                Box(
-                                    modifier = Modifier
-                                        .clip(CircleShape)
-                                        .size(44.dp)
-                                        .background(if (isSelected) Color.Green else Color.LightGray),
-                                    contentAlignment = Alignment.Center
+                            Box(
+                                modifier = Modifier
+                                    .clip(CircleShape)
+                                    .size(44.dp)
+                                    .background(if (isSelected) Color.Green else Color.LightGray),
+                                contentAlignment = Alignment.Center
 
-                                ) {
-                                    Icon(
-                                        modifier = Modifier.size(24.dp),
-                                        painter = painterResource(id = destination.icon),
-                                        contentDescription = destination.label,
-                                        tint = Color.Black
-                                    )
-                                    }
-                                }
-                                if (isSelected) {
-                                    Text(
-                                        text = destination.label,
-                                        style = MaterialTheme.typography.bodyMedium,
-                                        color = if (isSelected) Color.White else Color.Transparent,
-                                        fontSize = 14.sp
-                                    )
-                                }
-
+                            ) {
+                                Icon(
+                                    modifier = Modifier.size(24.dp),
+                                    painter = painterResource(id = destination.icon),
+                                    contentDescription = destination.label,
+                                    tint = Color.Black
+                                )
+                            }
                         }
-                    },
-                    label =null ,
-                    colors = NavigationBarItemColors(
-                        selectedIconColor = Color.Transparent,
-                        selectedTextColor = MaterialTheme.colorScheme.onPrimary,
-                        selectedIndicatorColor = Color.Transparent,
-                        unselectedIconColor = MaterialTheme.colorScheme.onSurfaceVariant,
-                        unselectedTextColor = Color.Black,
-                        disabledIconColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.38f),
-                        disabledTextColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.38f)
-                    ),
-                )
-            }
+                        if (isSelected) {
+                            Text(
+                                text = destination.label,
+                                style = MaterialTheme.typography.bodyMedium,
+                                color = if (isSelected) Color.White else Color.Transparent,
+                                fontSize = 14.sp
+                            )
+                        }
+                    }
+                },
+                label = null,
+                colors = NavigationBarItemColors(
+                    selectedIconColor = Color.Transparent,
+                    selectedTextColor = MaterialTheme.colorScheme.onPrimary,
+                    selectedIndicatorColor = Color.Transparent,
+                    unselectedIconColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                    unselectedTextColor = Color.Black,
+                    disabledIconColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.38f),
+                    disabledTextColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.38f)
+                ),
+            )
         }
     }
-
+}
 
 @Preview()
 @Composable()
